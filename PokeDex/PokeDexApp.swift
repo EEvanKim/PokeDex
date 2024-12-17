467228b24db5d2192788d22847485389362aa46c
Evan Kim <jekkej08@gmail.com>
2024-12-17 14:47:43 -0500

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Evan Kim (student LM) on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
