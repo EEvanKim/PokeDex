7375002e6215b14c821fec40fef397c32015bbac
Evan Kim <jekkej08@gmail.com>
2025-01-02 14:57:10 -0500

    started sprite stuff

--- PokeDex/Pokedex.swift
+++ PokeDex/Pokedex.swift
@@ -32,3 +32,16 @@ struct Pokemon: Codable{
 extension Pokemon: Identifiable{
     var id: String{name ?? ""}
 }
+struct FetchSpriteokemons{
+    var response: Response = Response()
+    
+    mutating func getData() async{
+        let URLString = "https://newsapi.org/v2/everything?domains=wsj.com&language=en&apiKey=948d8f533bf6439d94f891fae25ccf45"
+        
+        guard let url = URL(string: URLString) else {return}
+        guard let (data, _) = try? await URLSession.shared.data(from: url) else {return}
+        
+        guard let r = try? JSONDecoder().decode(Response.self, from: data) else {return}
+        response = r
+    }
+}
