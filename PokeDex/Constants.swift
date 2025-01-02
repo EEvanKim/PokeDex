305d28b6e1071afeb06b80358e26489d828a6d31
Finn <FinnOlshan@gmail.com>
2025-01-02 14:54:39 -0500

    constant content

--- PokeDex/Constants.swift
+++ PokeDex/Constants.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct Constants {
+    static let buttonFont: Font = Font(UIFont(name: "Helvetica-Neue", size: 24) ?? UIFont.systemFont(ofSize: 24))
+    static let textFont: Font = Font(UIFont(name: "Helvetica-Neue", size: 16) ?? UIFont.systemFont(ofSize: 16))
+    
+}
+
+extension Color{
+    static let backgroundBlue = Color("Background")
+    static let borderBlue = Color("Border")
+    static let greenFont = Color("Font")
+}
