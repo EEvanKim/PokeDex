5c0d73fc71b3df0b937df3822d43d4aadca71520
Evan Kim <jekkej08@gmail.com>
2025-01-02 15:04:07 -0500

    test

--- PokeDex/Pokedex.swift
+++ PokeDex/Pokedex.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct FetchDataPokemons{
-    var response: Response = Response()
+    var kanto: Kanto = Kanto()
     
     mutating func getData() async{
         let URLString = "https://newsapi.org/v2/everything?domains=wsj.com&language=en&apiKey=948d8f533bf6439d94f891fae25ccf45"
@@ -15,12 +15,12 @@ struct FetchDataPokemons{
         guard let url = URL(string: URLString) else {return}
         guard let (data, _) = try? await URLSession.shared.data(from: url) else {return}
         
-        guard let r = try? JSONDecoder().decode(Response.self, from: data) else {return}
-        response = r
+        guard let k = try? JSONDecoder().decode(Kanto.self, from: data) else {return}
+        kanto = k
     }
 }
 
-struct Response: Codable{
+struct Kanto: Codable{
     var results: [Pokemon] = []
 }
 
