f5dbdde300c4b62f1bc380c04453f5b7282beee3
thunderlight <thunderlight4800@gmail.com>
2025-01-10 18:28:42 -0500

    O)AIW(UOGLY

--- PokeDex/ContentView.swift
+++ PokeDex/ContentView.swift
@@ -2,14 +2,17 @@
 //  ContentView.swift
 //  PokeDex
 //
-//  Created by Evan Kim (student LM) on 12/17/24.
-//
+//  Created by 12345678910 (student LM) on 0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0/0.
+//  (im going insane)
 
 import SwiftUI
 
 struct ContentView: View {
     @State private var fetchPokemon = FetchDataPokemons()
     @State private var pokeSprites: [String: String] = [:]
+    @State private var pokedex: [String: String] = [:]
+    @State var url2: String
+    @State var pokedexEntry: String
     var body: some View {
         NavigationView {
             ZStack {
@@ -17,9 +20,49 @@ struct ContentView: View {
                     .foregroundColor(Color.backgroundRed)
                     .edgesIgnoringSafeArea(.all)
                 ScrollView {
+                    Text("--------------------")
+                        .padding()
+                        .font(Constants.pixelBoldFont)
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("! POKEDEX !")
+                        .font(Constants.pixelBoldFont)
+                        .foregroundColor(Color.white)
+                    Text("--------------------")
+                        .padding()
+                        .font(Constants.pixelBoldFont)
                     ForEach(fetchPokemon.kanto.results) { poke in
                         VStack {
-                            NavigationLink {
+                            NavigationLink {if let spriteURL = pokeSprites[poke.name] {
+                                Text(poke.name)
+                                    .font(Constants.pixelBoldFont)
+                                    .foregroundColor(Color.black)
+                                AsyncImage(url: URL(string: spriteURL)) { phase in
+                                        switch phase {
+                                        case .empty:
+                                            ProgressView()
+                                        case .success(let image):
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                        case .failure:
+                                            Image(systemName: "questionmark")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                        }
+                                    }
+                                } else {
+                                    ProgressView()
+                                        .frame(height: 200)
+                                }
+                                Text("--------------------")
+                                    .padding()
+                                    .font(Constants.pixelBoldFont)
+                                Text(pokedex[poke.name] ?? pokedexEntry)
+                                    .font(Constants.pixelBoldFont)
+                                    .foregroundColor(Color.black)
+                            } label: {
                                 if let spriteURL = pokeSprites[poke.name] {
                                     AsyncImage(url: URL(string: spriteURL)) { phase in
                                         switch phase {
@@ -29,27 +72,16 @@ struct ContentView: View {
                                             image
                                                 .resizable()
                                                 .aspectRatio(contentMode: .fit)
-                                                .cornerRadius(25)
-                                                .frame(height: 400)
                                         case .failure:
                                             Image(systemName: "questionmark")
                                                 .resizable()
                                                 .aspectRatio(contentMode: .fit)
-                                                .cornerRadius(25)
-                                                .frame(height: 50)
                                         }
                                     }
                                 } else {
                                     ProgressView()
                                         .frame(height: 200)
                                 }
-                                Text(poke.name)
-                                    .font(Constants.pixelBoldFont)
-                                    .foregroundColor(Color.black)
-                            } label: {
-                                Text(poke.name)
-                                    .font(Constants.pixelBoldFont)
-                                    .foregroundColor(Color.white)
                             }
                             Text("--------------------")
                                 .padding()
@@ -60,18 +92,20 @@ struct ContentView: View {
                             if let spriteURL = await fetchPokemon.getSpriteData(pokeURL: poke.url) {
                                 pokeSprites[poke.name] = spriteURL
                             }
+                            let pokeid = await fetchPokemon.getPokeDetails(detailsURL: poke.url)
+                                if let pokeid = pokeid {
+                                    let speciesURL = "https://pokeapi.co/api/v2/pokemon-species/\(pokeid)/"
+                                    if let pokedexEntry = await fetchPokemon.getPokedexEntry(speciesURL: speciesURL) {
+                                        pokedex[poke.name] = pokedexEntry
+                                    } else {
+                                        pokedex[poke.name] = "No Pokédex entry available."
+                                    }
+                                }
                         }
                     }
                 }
                 .task {
                     await fetchPokemon.getData()
-                    VStack {
-                        Image(systemName: "globe")
-                            .imageScale(.large)
-                            .foregroundStyle(.tint)
-                        Text("POKE")
-                            .font(Constants.pixelBoldFont)
-                    }
             }
             }
         }
@@ -80,5 +114,5 @@ struct ContentView: View {
 
 
 #Preview {
-    ContentView()
+    ContentView(url2: "", pokedexEntry: "")
 }
--- PokeDex/Pokedex.swift
+++ PokeDex/Pokedex.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-struct FetchDataPokemons {
+struct FetchDataPokemons: Codable{
     var kanto: Kanto = Kanto()
     
     mutating func getData() async {
@@ -32,6 +32,37 @@ struct FetchDataPokemons {
         guard let spriteData = try? JSONDecoder().decode(PokemonDetail.self, from: data) else { return nil }
         return spriteData.sprites.front_default
     }
+    
+    mutating func getPokedexEntry(speciesURL: String) async -> String? {
+        guard !speciesURL.isEmpty else { return nil }
+        
+        guard let url = URL(string: speciesURL) else { return nil }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let speciesData = try JSONDecoder().decode(SpeciesDetail.self, from: data)
+            
+            if let flavorTextEntry = speciesData.flavor_text_entries.first(where: { $0.language.name == "en" }) {
+                return flavorTextEntry.flavor_text
+            }
+            
+            return nil
+        } catch {
+            print("Failed to fetch species data from \(speciesURL): \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func getPokeDetails(detailsURL: String) async -> Int? {
+        guard !detailsURL.isEmpty else { return nil }
+        
+        let URLString = detailsURL
+        
+        guard let url = URL(string: URLString) else { return nil }
+        guard let (data, _) = try? await URLSession.shared.data(from: url) else { return nil }
+        
+        guard let spriteData = try? JSONDecoder().decode(PokemonDetail.self, from: data) else { return nil }
+        return spriteData.id
+    }
 }
 
 struct Kanto: Codable {
@@ -43,12 +74,30 @@ struct Pokemon: Codable, Identifiable {
     var url: String
     
     var id: String { name }
+    var pokedexEntry: String? = nil
 }
 
 struct PokemonDetail: Codable {
     var sprites: Sprite
+    var id: Int
 }
 
 struct Sprite: Codable {
     var front_default: String?
 }
+
+struct SpeciesDetail: Codable {
+    var flavor_text_entries: [Flavor_Text_Entries]
+}
+
+struct Flavor_Text_Entries: Codable {
+    let flavor_text: String
+    let language: Language
+}
+
+struct Language: Codable {
+    let name: String
+}
+
+
+//https://pokeapi.co/api/v2/pokemon-species/1/
